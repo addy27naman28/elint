f2ae88bf50ea8745b8437e9f3dfe9528d09f76ed
addy27naman28 <162831925+addy27naman28@users.noreply.github.com>
2024-03-10 00:28:06 +0530

    repo code push

---
+++ src/main/java/com/ai/elint/ElintApplication.java
@@ -0,0 +1,13 @@
+package com.ai.elint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai/elint/ElintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.elint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
